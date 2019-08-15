d03d78f3eaa94241621465fe2e993f7fedaebf7e
johnnascimento <jlenonns@gmail.com>
2019-08-15 09:14:31 -0300

    first test

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('Knock out script working sound');
+
+const viewMode = {
+  user: {
+    firstName: 'John',
+    lastName: 'Nascimento',
+    age: 28
+  }
+};
+
+
+ko.applyBindings(viewMode);
