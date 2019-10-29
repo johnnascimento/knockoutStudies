87f2cdfbb02783a732d7115d33e753190d78d1da
John Lenon Nascimento da Silva <jnascimento@itelios.com>
2019-10-29 08:34:44 -0300

    Knockout stuff

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ console.log('Knock out script working sound');
 
 const viewMode = {
   user: {
-    firstName: 'John',
-    lastName: 'Nascimento',
-    age: 28
+    firstName: ko.observable('John'),
+    lastName: ko.observable('Nascimento'),
+    age: ko.observable(28),
+    cartValue: ko.observable(25)
   }
 };
 
-
 ko.applyBindings(viewMode);
+viewMode.user.lastName('Lenon');
+
+var myObservableArray = ko.observableArray();    // Initially an empty array
+myObservableArray.push('Some value');            // Adds the value and notifies observers
